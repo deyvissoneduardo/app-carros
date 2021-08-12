f5821a66e39286e839443cbef003d74d0ce861e6
Deyvisson Eduardo <deyvissonarouche@gmail.com>
2021-08-12 15:16:26 -0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'title',
       theme: ThemeData(
           primarySwatch: Colors.blue,
           brightness: Brightness.light,
